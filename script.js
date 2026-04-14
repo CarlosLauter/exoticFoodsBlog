dd7350df24ed989e742cd3871a4b18f23c3c3be6
CarlosLauter <toninhoyuki@gmail.com>
2026-04-13 23:27:25 -0300

    Adicionando notas e novos posts

--- script.js
+++ script.js
@@ -2,30 +2,74 @@ const foods = [
 {
     nome: 'Takis Azul',
     id: 0,
-    tags: ['doritos', 'salgadinho', 'snack', 'takis', 'azul', 'salgado', 'salgados', 'salgada', 'salgadas'],
+    tags: ['doritos', 'salgadinho', 'snack', 'takis', 'azul', 'salgado', 'salgados', 'salgada', 'salgadas', 'picante'],
     img: 'assets/images/blueTakis.jpg',
-    link: 'posts/blueTakis.html'
+    link: 'posts/blueTakis.html',
+    notas: {
+        carlos: 0,
+        mahena: 0,
+        final: 0,
+    }
 },
 {
     nome: 'Baly Caipirinha',
     id: 1,
     tags: ['baly', 'bebida', 'bebidas', 'energetico', 'verde'],
     img: 'assets/images/balyCaipirinha.jpg',
-    link: 'posts/balyCaipirinha.html'
+    link: 'posts/balyCaipirinha.html',
+    notas: {
+        carlos: 0,
+        mahena: 0,
+        final: 0,
+    }
 },
 {
     nome: 'Bubble Mix',
     id: 2,
     tags: ['bubble', 'mix', 'cha', 'bebida', 'bebidas', 'rosa'],
     img: 'assets/images/bubbleMix.jpg',
-    link: 'posts/bubbleMix.html'
+    link: 'posts/bubbleMix.html',
+    notas: {
+        carlos: 0,
+        mahena: 0,
+        final: 0,
+    }
 },
 {
     nome: 'Devi Frutas Vermelhas',
     id: 3,
     tags: ['refri', 'refrigerante', 'gas', 'bebida', 'bebidas', 'frutas', 'vermelhas', 'devi', 'vermelho'],
     img: 'assets/images/deviRefrigeranteFrutasVermelhas.jpg',
-    link: 'posts/deviFrutasVermelhas.html'
+    link: 'posts/deviFrutasVermelhas.html',
+    notas: {
+        carlos: 1.5,
+        mahena: 3.5,
+        final: 2.5
+    }
+},
+{
+    nome: 'Devi Guaraná e Açaí',
+    id: 4,
+    tags: ['refri', 'refrigerante', 'gas', 'bebida', 'bebidas', 'frutas', 'guarana', 'acai', 'roxo'],
+    img: 'assets/images/deviRefrigeranteGuaranaAçai.jpg',
+    link: 'posts/deviGuaranaAcai.html',
+    notas: {
+        carlos: 3,
+        mahena: 1,
+        final: 2
+    }
+},
+{
+    nome: 'Sucrilhos de Banana',
+    id: 5,
+    tags: ['sucrilhos', 'doce', 'doces', 'banana', 'fruta', 'crocante', 'amarelo'],
+    img: 'assets/images/sucrilhosBanana.jpg',
+    link: 'posts/sucrilhosBanana.html',
+    notas: {
+        carlos: 5,
+        mahena: 5,
+        final: 5
+    }
 }
 ]
 
@@ -116,4 +160,19 @@ function verificarEnter(event) {
 
         document.getElementById('input').value = ''
     }
+}
+
+function gerarEstrelas(nota) {
+    const total = 5
+    let html = ''
+
+    const cheias = Math.floor(nota)
+    const meia = nota % 1 >= 0.5 ? 1 : 0
+    const vazias = total - cheias - meia
+
+    for (let i = 0; i < cheias; i++) html += '★'
+    if (meia) html += '⯨'
+    for (let i = 0; i < vazias; i++) html += '☆'
+
+    return html
 }
